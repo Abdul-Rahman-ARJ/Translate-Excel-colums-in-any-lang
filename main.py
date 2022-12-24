656aac678376cc502bd1201b3e4d5540430c4c70
Abdul Rahman <93648056+Abdul-Rahman-ARJ@users.noreply.github.com>
2022-12-24 23:34:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ print(translated_text.text)
 # translated_text = translator.translate('안녕하세요.', dest='ja')
 # print(translated_text.text)
 #
-# translated_text = translator.translate('veritas lux mea', src='la')
-# print(translated_text.text)
+translated_text = translator.translate('veritas lux mea', src='la')
+print(translated_text.text)
